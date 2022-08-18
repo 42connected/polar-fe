49d62fbe448bd2f7ca3c61449df609ba4b53ad31
dkjefilsjl <6792kjs@naver.com>
2022-08-18 14:40:09 +0900

    POL-127 GlobalStyle

---
+++ src/styles/styled.d.ts
@@ -0,0 +1,47 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    shadow: {
+      defaultShadow: string;
+      buttonShadow: string;
+    };
+    colors: {
+      polarMain: string;
+      polarSimpleMain: string;
+      polarBackground: string;
+      polarBrightMain: string;
+      polarGray: string;
+      grayOne: string;
+      grayTwo: string;
+      grayThree: string;
+      grayFour: string;
+      grayFive: string;
+      graySix: string;
+      backgoundWhite: string;
+      blackOne: string;
+      blackTwo: string;
+      blackThree: string;
+      inputBoxBackgound: string;
+      fontGray: string;
+      Red: string;
+      Yello: string;
+    };
+    font: {
+      titleSmall: string;
+      titleMedium: string;
+      titleLarge: string;
+      titleGiant: string;
+      subTitleSmall: string;
+      subTitleMiddle: string;
+      bodySmall: string;
+      bodyMiddle: string;
+    };
+    fontColor: {
+      titleColor: string;
+      blueColor: string;
+      whiteColor: string;
+      grayColor: string;
+    };
+  }
+}
