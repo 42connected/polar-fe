f6876296dd31d98bd6dd5414c57339d6d5f487f0
jooocy <hyean2302@gmail.com>
2022-08-21 17:04:38 +0900

    POL-axios : lefthook

--- src/context/axios-interface.ts
+++ src/context/axios-interface.ts
@@ -1,9 +1,9 @@
-import axios from "axios";
+import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: process.env.BASE_BACKEND_URL,
-    withCredentials: true,
-    headers: {
-        "Content-Type": "application/json"
-    }
+  baseURL: process.env.BASE_BACKEND_URL,
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
