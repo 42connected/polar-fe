eac27c05c3c2303ee1448a9281f5e7136a70c48c
jooocy <hyean2302@gmail.com>
2022-08-22 12:51:45 +0900

    POL-164: lefthook

--- src/context/cookies.ts
+++ src/context/cookies.ts
@@ -1,19 +1,18 @@
-import { Cookies} from 'react-cookie';
+import { Cookies } from 'react-cookie';
 
-const cookies = new Cookies()
+const cookies = new Cookies();
 
 export const setCookie = (_: any, value: string, option?: any) => {
-    cookies.set("accessToken", value, {...option})
-}
+  cookies.set('accessToken', value, { ...option });
+};
 
 export const getCookie = (_: any) => {
-    return cookies.get("accessToken")
-}
+  return cookies.get('accessToken');
+};
 
 export const removeCookie = (_: any) => {
-    cookies.remove("accessToken")
-}
-
+  cookies.remove('accessToken');
+};
 
 /* option example */
 // { path: '/', secure: true, sameSite: "none" }
