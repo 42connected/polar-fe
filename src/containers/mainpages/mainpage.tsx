a928ec007a6c7e3220015f85fdc3e2258e4b0a2a
dkjefilsjl <6792kjs@naver.com>
2022-08-26 09:19:25 +0900

    Fix:Main

--- src/containers/mainpages/mainpage.tsx
+++ src/containers/mainpages/mainpage.tsx
@@ -1,6 +1,6 @@
 import styled, { css } from 'styled-components';
 import theme from '../../styles/theme';
-import img from '../../assets/image/logo/phone.png';
+//import img from '../../assets/image/logo/phone.png';
 import { keywordsPro } from './mainpage-getkeyword';
 import { useEffect, useState } from 'react';
 import { axiosInstance } from '../../context/axios-interface';
@@ -8,18 +8,16 @@ import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faAnglesUp,
-  faBezierCurve,
-  faBrain,
   faBuildingUser,
   faCode,
   faFlagCheckered,
   faGraduationCap,
   faLaptopCode,
   faLightbulb,
-  faMemory,
   faSitemap,
 } from '@fortawesome/free-solid-svg-icons';
 import { debounce } from '@mui/material';
+import img from '../../assets/image/logo/desc.png';
 
 const MainContainer = styled.div`
   background-color: ${theme.colors.polarBackground};
@@ -31,45 +29,110 @@ const MainContainer = styled.div`
   display: grid;
   grid-template-rows: 200px 150px 200px;
   grid-template-columns: repeat(2, minmax(400px, auto));
+  transition: all 0.25s ease-in-out;
   grid-template-areas:
     'img title'
     'img icon1'
     'img icon2';
   text-align: center;
   justify-content: center;
-  grid-column-gap: 20rem;
-  transition: all 0.25s ease-in-out;
+  grid-column-gap: 18rem;
   border-radius: 10px;
 `;
 
 const MainContainer2 = styled.div`
   background-color: ${theme.colors.polarBackground};
   left: 0;
-  ${theme.fontSize.sizeExtraSmall};
+  ${theme.fontSize.sizeSmall};
   ${theme.font.sebangGothic};
   height: 120vh;
   width: 100%;
   display: grid;
-  grid-template-rows: 200px 150px 150px 300px;
+  grid-template-rows: 500px 200px 150px 150px;
   grid-template-areas:
+    'img'
     'title'
     'icon1'
-    'icon2'
-    'img';
+    'icon2';
   text-align: center;
   justify-content: center;
   transition: all 0.25s ease-in-out;
   border-radius: 10px;
+  ${theme.fontSize.sizeMedium};
 `;
 
 const MainImageStyle = styled.div`
+  box-sizing: border-box;
+  background-color: ${theme.colors.backgoundWhite};
   border-radius: 20%;
+  height: 40rem;
+  width: 60rem;
   grid-auto-flow: dense;
   margin-top: 15rem;
   align-items: center;
   justify-content: center;
+  padding-top: 3rem;
   text-align: center;
   grid-area: img;
+  display: grid;
+  grid-template-rows: 300px 100px;
+  transition: all 0.25s ease-in-out;
+`;
+
+const ImageGrid1 = styled.div`
+  line-height: 4rem;
+  grid-row-start: 1;
+`;
+
+const ImageGrid2 = styled.div`
+  grid-row-start: 2;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  flex-direction: column;
+`;
+
+const ImageStyle = styled.span`
+  display: flex;
+`;
+
+const ImageStyle2 = styled.span`
+  margin-left: 1rem;
+`;
+
+const ClickedSwapButton = styled.button`
+  border-radius: 10rem;
+  background-color: ${theme.colors.polarSimpleMain};
+  color: ${theme.colors.polarSimpleMain};
+  height: 0.8rem;
+  width: 6rem;
+  word-spacing: 1rem;
+  border: none;
+`;
+
+const SwapButton = styled.button`
+  box-sizing: border-box;
+  border-radius: 100rem;
+  height: 0.8rem;
+  width: 0.8rem;
+  word-spacing: 1rem;
+  border: none;
+  background-color: ${theme.colors.grayFour};
+  color: ${theme.colors.graySix};
+`;
+
+const TextUnder = styled.div`
+  box-sizing: border-box;
+  padding-bottom: 1rem;
+  border-bottom: 1px solid ${theme.colors.polarSimpleMain};
+  text-underline-offset: 1rem;
+  ${theme.fontSize.sizeExtraMedium};
+`;
+
+const TextStyle = styled.div`
+  margin-top: 1rem;
+  ${theme.fontSize.sizeMedium};
 `;
 
 const TitleStyle = styled.div`
@@ -86,17 +149,19 @@ const TitleStyle = styled.div`
 const ClickContainer = styled.div`
   display: grid;
   grid-template-columns: 100px 100px 100px 100px;
+  transition: all 0.25s ease-in-out;
   grid-template-rows: 100px;
   text-align: center;
   justify-content: center;
   align-items: center;
-  transition: all 0.25s ease-in-out;
   margin-top: 5rem;
   grid-row-gap: 0;
 `;
 
 const IconImageStyle = styled.div<{ colStart: number; rowStart: number }>`
   display: flex;
+  border-radius: 50%;
+  overflow: hidden;
   grid-column-start: colStart;
   grid-row-start: rowStart;
   text-align: center;
@@ -139,7 +204,59 @@ const Mainpage = () => {
     width: window.innerWidth,
     height: window.innerHeight,
   });
+  const [text, setText] = useState(
+    <div>
+      <TextUnder>이용안내 - 카뎃</TextUnder>
+      <br />
+      1.멘토의 멘토링 상태 확인하고 멘토링 신청 버튼 클릭
+      <br />
+      2.만남 일정과 정보를 작성하고 제출
+      <br />
+      3.마이페이지에서 만남 상태 확인 가능
+      <br />
+      4.멘토링이 확정, 취소되면 카뎃에게 알림 메일 발송
+      <br />
+      5.장소협의 후 만남 일정 시간에 멘토링 진행
+    </div>,
+  );
+  const [isleft, setIsLeft] = useState(true);
   const [isMobile, setIsMobile] = useState(false);
+  const textSwap1 = () => {
+    return (
+      setText(
+        <div>
+          <TextUnder>이용안내 - 카뎃</TextUnder>
+          <br />
+          1.멘토의 멘토링 상태 확인하고 멘토링 신청 버튼 클릭
+          <br />
+          2.만남 일정과 정보를 작성하고 제출
+          <br />
+          3.마이페이지에서 만남 상태 확인 가능
+          <br />
+          4.멘토링이 확정, 취소되면 카뎃에게 알림 메일 발송
+          <br />
+          5.장소협의 후 만남 일정 시간에 멘토링 진행
+        </div>,
+      ),
+      setIsLeft(true)
+    );
+  };
+  const textSwap2 = () => {
+    return (
+      setText(
+        <div>
+          <TextUnder>이용안내 - 멘토</TextUnder>
+          <br />
+          1. 카뎃의 멘토링 신청 시 알림 메일 발송 <br />
+          2. 마이페이지에서 만남 상태 결정 가능 <br />
+          3. 멘토링이 확정, 취소되면 카뎃에게 알림 메일 발송 <br />
+          4. 장소협의 후 만남 일정 시간에 멘토링 진행 <br />
+          5. 멘토링 진행 후 보고서 작성 가능
+        </div>,
+      ),
+      setIsLeft(false)
+    );
+  };
   const handleResize = debounce(() => {
     setWindowSize({
       width: window.innerWidth,
@@ -174,7 +291,30 @@ const Mainpage = () => {
         <div>
           <MainContainer>
             <MainImageStyle>
-              <img src={img} style={imagestyle1} className="App-logo" />
+              <ImageGrid1>{text}</ImageGrid1>
+              <ImageGrid2>
+                {isleft ? (
+                  <div>
+                    <ImageStyle>
+                      <ClickedSwapButton
+                        onClick={textSwap1}
+                      ></ClickedSwapButton>
+                      <ImageStyle2></ImageStyle2>
+                      <SwapButton onClick={textSwap2}></SwapButton>
+                    </ImageStyle>
+                  </div>
+                ) : (
+                  <div>
+                    <ImageStyle>
+                      <SwapButton onClick={textSwap1}></SwapButton>
+                      <ImageStyle2></ImageStyle2>
+                      <ClickedSwapButton
+                        onClick={textSwap2}
+                      ></ClickedSwapButton>
+                    </ImageStyle>
+                  </div>
+                )}
+              </ImageGrid2>
             </MainImageStyle>
             <TitleStyle>
               나에게 꼭 맞는 <MainBlueBody> 멘토 </MainBlueBody> 선택하기
@@ -240,7 +380,30 @@ const Mainpage = () => {
         <div>
           <MainContainer2>
             <MainImageStyle>
-              <img src={img} style={imagestyle1} className="App-logo" />
+              <ImageGrid1>{text}</ImageGrid1>
+              <ImageGrid2>
+                {isleft ? (
+                  <div>
+                    <ImageStyle>
+                      <ClickedSwapButton
+                        onClick={textSwap1}
+                      ></ClickedSwapButton>
+                      <ImageStyle2></ImageStyle2>
+                      <SwapButton onClick={textSwap2}></SwapButton>
+                    </ImageStyle>
+                  </div>
+                ) : (
+                  <div>
+                    <ImageStyle>
+                      <SwapButton onClick={textSwap1}></SwapButton>
+                      <ImageStyle2></ImageStyle2>
+                      <ClickedSwapButton
+                        onClick={textSwap2}
+                      ></ClickedSwapButton>
+                    </ImageStyle>
+                  </div>
+                )}
+              </ImageGrid2>
             </MainImageStyle>
             <TitleStyle>
               나에게 꼭 맞는 <MainBlueBody> 멘토 </MainBlueBody> 선택하기
