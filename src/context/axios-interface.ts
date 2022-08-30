520be63b495c1e3138836c88c2ebce9927ae1e57
kanghyki <kanghyki@gmail.com>
2022-08-30 18:35:41 +0900

    Fix:

--- src/context/axios-interface.ts
+++ src/context/axios-interface.ts
@@ -4,7 +4,7 @@ export const axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_BASE_BACKEND_URL,
   withCredentials: true,
   headers: {
-    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Origin': `${process.env.REACT_APP_ORIGIN}`,
     'Content-Type': 'application/json',
   },
 });
