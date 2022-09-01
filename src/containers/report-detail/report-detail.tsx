31f465239d7c4d94f5d26631c2349746e2f10963
dkjefilsjl <6792kjs@naver.com>
2022-09-01 14:13:43 +0900

    Fix:report list

--- src/containers/report-detail/report-detail.tsx
+++ src/containers/report-detail/report-detail.tsx
@@ -3,7 +3,7 @@ import ReactToPrint from 'react-to-print';
 import styled from 'styled-components';
 import { axiosInstance } from '../../context/axios-interface';
 import theme from '../../styles/theme';
-import { getReportId, reportsPro } from './getreportdata';
+import { reportsPro } from './getreportdata';
 import ino1 from '../../assets/image/logo/ino1.png';
 import ino2 from '../../assets/image/logo/ino2.png';
 import {
@@ -50,21 +50,25 @@ import {
 import AuthStore from '../../states/auth/AuthStore';
 import { useParams, useSearchParams } from 'react-router-dom';
 import { debounce } from '@mui/material';
-import { useQuery } from 'react-query';
+import { isConstructorDeclaration } from 'typescript';
 
-const ReportpageStyle = styled.div`
+const ReportpageStyle = styled.div<{
+  height: number;
+}>`
   background-color: ${theme.colors.backgoundWhite};
   margin-left: 40rem;
   width: 100%;
-  height: 130vw;
+  height: ${props => props.height * 250 + 'vh'};
   margin-left: 0;
 `;
 
-const ReportpageStyle2 = styled.div`
+const ReportpageStyle2 = styled.div<{
+  height: number;
+}>`
   background-color: ${theme.colors.backgoundWhite};
   margin-left: 40rem;
   width: 100%;
-  height: 260vw;
+  height: ${props => props.height * 300 + 'vh'};
   margin-left: 0;
 `;
 
@@ -115,107 +119,118 @@ const SimpleComponent = (props: {
   reportDatas: reportsPro[];
 }) => {
   const { printRef, reportDatas } = props;
-
   return (
     <div ref={printRef}>
-      {reportDatas?.map((reportdata: reportsPro, index: number) => {
+      {reportDatas?.map((reportdata: reportsPro, index: number, origin) => {
+        console.log('seoyeon');
+        console.log('index: ' + index);
         return (
-          <ReportContainer index={index}>
-            <Title>
-              <Titleplus>42 SEOUL</Titleplus> 멘토링 보고서(멘토용)
-            </Title>
-            <NoneValue1></NoneValue1>
-            <SubTitle1>구분</SubTitle1>
-            <MiniTitle1>공통과정</MiniTitle1>
-            <MiniTitle2>심화과정</MiniTitle2>
-            <NoneValue2></NoneValue2>
-            <SubTitle2>날짜</SubTitle2>
-            <DateBox>
-              {(reportdata?.mentoringLogs.meetingAt[0] + '').substring(0, 10)}
-            </DateBox>
-            <SubTitle8>시간</SubTitle8>
-            <TimeBox>
-              {(reportdata?.mentoringLogs.meetingAt[0] + '').substring(11, 16)}{' '}
-              ~{' '}
-              {(reportdata?.mentoringLogs.meetingAt[1] + '').substring(11, 16)}
-            </TimeBox>
-            <SubTitle3>장소</SubTitle3>
-            <PlaceBox>{reportdata?.place}</PlaceBox>
-            <SubTitle4>멘토이름</SubTitle4>
-            <MentoNameBox>{reportdata?.mentors.name}</MentoNameBox>
-            <SubTitle9>멘티이름</SubTitle9>
-            <KadetNameBox>{reportdata?.cadets.name}</KadetNameBox>
-            <NoneValue3></NoneValue3>
-            <SubTitle5>멘토링개요</SubTitle5>
-            <ContentTitle1>주제</ContentTitle1>
-            <ContentBody1>{reportdata?.topic}</ContentBody1>
-            <ContentTitle2>내용</ContentTitle2>
-            <ContentBody2>{reportdata?.content}</ContentBody2>
-            <ContentTitle3>
-              교육생
-              <br />
-              에게
-              <br /> 남기는 말
-            </ContentTitle3>
-            <ContentBody3>{reportdata?.feedbackMessage}</ContentBody3>
-            <Number1> - 1 -</Number1>
-            <SubTitle6>증빙사진</SubTitle6>
-            <ContentBody4>
-              {reportdata?.imageUrl[0] ? (
-                <img src={reportdata?.imageUrl[0]} style={imagestyle1} />
+          <div key={index}>
+            <ReportContainer index={index}>
+              <Title>
+                <Titleplus>42 SEOUL</Titleplus> 멘토링 보고서(멘토용)
+              </Title>
+              <NoneValue1></NoneValue1>
+              <SubTitle1>구분</SubTitle1>
+              <MiniTitle1>공통과정</MiniTitle1>
+              <MiniTitle2>심화과정</MiniTitle2>
+              <NoneValue2></NoneValue2>
+              <SubTitle2>날짜</SubTitle2>
+              <DateBox>
+                {(reportdata?.mentoringLogs.meetingAt[0] + '').substring(0, 10)}
+              </DateBox>
+              <SubTitle8>시간</SubTitle8>
+              <TimeBox>
+                {(reportdata?.mentoringLogs.meetingAt[0] + '').substring(
+                  11,
+                  16,
+                )}{' '}
+                ~{' '}
+                {(reportdata?.mentoringLogs.meetingAt[1] + '').substring(
+                  11,
+                  16,
+                )}
+              </TimeBox>
+              <SubTitle3>장소</SubTitle3>
+              <PlaceBox>{reportdata?.place}</PlaceBox>
+              <SubTitle4>멘토이름</SubTitle4>
+              <MentoNameBox>{reportdata?.mentors.name}</MentoNameBox>
+              <SubTitle9>멘티이름</SubTitle9>
+              <KadetNameBox>{reportdata?.cadets.name}</KadetNameBox>
+              <NoneValue3></NoneValue3>
+              <SubTitle5>멘토링개요</SubTitle5>
+              <ContentTitle1>주제</ContentTitle1>
+              <ContentBody1>{reportdata?.topic}</ContentBody1>
+              <ContentTitle2>내용</ContentTitle2>
+              <ContentBody2>{reportdata?.content}</ContentBody2>
+              <ContentTitle3>
+                교육생
+                <br />
+                에게
+                <br /> 남기는 말
+              </ContentTitle3>
+              <ContentBody3>{reportdata?.feedbackMessage}</ContentBody3>
+              <Number1> - 1 -</Number1>
+              <SubTitle6>증빙사진</SubTitle6>
+              <ContentBody4>
+                {reportdata?.imageUrl[0] ? (
+                  <img src={reportdata?.imageUrl[0]} style={imagestyle1} />
+                ) : (
+                  ''
+                )}
+                {reportdata?.imageUrl[1] ? (
+                  <img src={reportdata?.imageUrl[1]} style={imagestyle1} />
+                ) : (
+                  ''
+                )}
+                {reportdata?.imageUrl[2] ? (
+                  <img src={reportdata?.imageUrl[2]} style={imagestyle1} />
+                ) : (
+                  ''
+                )}
+              </ContentBody4>
+              <SubTitle7>
+                멘토링 <br /> 피드백
+              </SubTitle7>
+              <ContentBody5>
+                멘토링에 대한 느낌을 점수로 적어주세요. (최고 5점, 최저 1점)
+                <br />
+                1. 교육생이 궁금한 것을 잘 정리해왔나요? (
+                {reportdata?.feedback1})
+                <br />
+                2. 교육생과 함께 한 시간이 만족스러웠나요? (
+                {reportdata?.feedback2})
+                <br />
+                3. 교육생이 전달한 내용을 잘 이해했나요? (
+                {reportdata?.feedback3})
+                <br />
+              </ContentBody5>
+              <ContentBody6>(재)이노베이션 아카데미 귀하</ContentBody6>
+              <Number2> - 2 -</Number2>
+              {reportdata?.cadets.isCommon ? (
+                <IsCommonBox> o </IsCommonBox>
               ) : (
-                ''
+                <IsCommonBox> </IsCommonBox>
               )}
-              {reportdata?.imageUrl[1] ? (
-                <img src={reportdata?.imageUrl[1]} style={imagestyle1} />
+              {reportdata?.cadets.isCommon ? (
+                <NotCommonBox> </NotCommonBox>
               ) : (
-                ''
+                <NotCommonBox> o </NotCommonBox>
               )}
-              {reportdata?.imageUrl[2] ? (
-                <img src={reportdata?.imageUrl[2]} style={imagestyle1} />
-              ) : (
-                ''
-              )}
-            </ContentBody4>
-            <SubTitle7>
-              멘토링 <br /> 피드백
-            </SubTitle7>
-            <ContentBody5>
-              멘토링에 대한 느낌을 점수로 적어주세요. (최고 5점, 최저 1점)
-              <br />
-              1. 교육생이 궁금한 것을 잘 정리해왔나요? ({reportdata?.feedback1})
-              <br />
-              2. 교육생과 함께 한 시간이 만족스러웠나요? (
-              {reportdata?.feedback2})
-              <br />
-              3. 교육생이 전달한 내용을 잘 이해했나요? ({reportdata?.feedback3})
-              <br />
-            </ContentBody5>
-            <ContentBody6>(재)이노베이션 아카데미 귀하</ContentBody6>
-            <Number2> - 2 -</Number2>
-            {reportdata?.cadets.isCommon ? (
-              <IsCommonBox> o </IsCommonBox>
-            ) : (
-              <IsCommonBox> </IsCommonBox>
-            )}
-            {reportdata?.cadets.isCommon ? (
-              <NotCommonBox> </NotCommonBox>
-            ) : (
-              <NotCommonBox> o </NotCommonBox>
-            )}
-            <ImgLogo1>
-              <img src={ino1} style={imagestyle2} className="report-image" />
-            </ImgLogo1>
-            <ImgLogo2>
-              <img src={ino2} style={imagestyle3} className="report-image" />
-            </ImgLogo2>
-            <ImgLogo3>
-              <img src={ino1} style={imagestyle2} className="report-image" />
-            </ImgLogo3>
-            <ImgLogo4>
-              <img src={ino2} style={imagestyle3} className="report-image" />
-            </ImgLogo4>
-          </ReportContainer>
+              <ImgLogo1>
+                <img src={ino1} style={imagestyle2} className="report-image" />
+              </ImgLogo1>
+              <ImgLogo2>
+                <img src={ino2} style={imagestyle3} className="report-image" />
+              </ImgLogo2>
+              <ImgLogo3>
+                <img src={ino1} style={imagestyle2} className="report-image" />
+              </ImgLogo3>
+              <ImgLogo4>
+                <img src={ino2} style={imagestyle3} className="report-image" />
+              </ImgLogo4>
+            </ReportContainer>
+          </div>
         );
       })}
     </div>
@@ -244,23 +259,20 @@ const ReportDetail = () => {
   }, 10);
 
   const getReports = async (reportId: string) => {
+    console.log('please');
     try {
       setLoading(true);
-      console.log(`${reportId}`);
       const save = await axiosInstance.get(`/reports/${reportId}`, {
         headers: {
           Authorization: `bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Im0tZW5nZW5nIiwicm9sZSI6ImJvY2FsIiwiaWF0IjoxNjYxOTI4ODk0LCJleHAiOjE2NjIwMTUyOTR9.PBBtkaFQt3bakkFro7zPMK_EWN40vJLjrt2po5r_SeE`,
         },
       });
       const data: reportsPro = save.data;
-      console.log(data);
-      tmp?.push(data);
-      console.log(tmp);
+      tmp.push(data);
       setreportDatas(tmp);
       setLoading(false);
     } catch (e) {
       console.log(e);
-      return e;
     }
   };
 
@@ -268,16 +280,19 @@ const ReportDetail = () => {
     window.addEventListener('resize', handleResize);
     setToken(AuthStore.getAccessToken());
     setRole(AuthStore.getUserRole());
-    reportIds?.map((reportId: string) => {
-      return getReports(reportId);
-    });
+    {
+      reportIds?.forEach((reportId: string) => {
+        getReports(reportId);
+        console.log('why??');
+      });
+    }
     return () => {
       window.removeEventListener('resize', handleResize);
     };
   }, []);
 
   return isMobile ? (
-    <ReportpageStyle2>
+    <ReportpageStyle2 height={reportDatas.length}>
       <ImgBody>
         <SimpleComponent printRef={componentRef} reportDatas={reportDatas} />
       </ImgBody>
@@ -291,7 +306,7 @@ const ReportDetail = () => {
       />
     </ReportpageStyle2>
   ) : (
-    <ReportpageStyle>
+    <ReportpageStyle height={reportDatas.length}>
       <ImgBody>
         <SimpleComponent printRef={componentRef} reportDatas={reportDatas} />
       </ImgBody>
