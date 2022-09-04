4424427ad277468498d2408fdb4baff89ea9fee1
Joo-hyun-Kang <autoba9687@gmail.com>
2022-09-04 19:43:11 +0900

    fix: 테스트용 url 제거

--- src/App.tsx
+++ src/App.tsx
@@ -64,25 +64,6 @@ const App = observer(() => {
           <Route path="/mentor-detail/:intraId" element={<MentorDetail />} />
           <Route path="/mentors/join" element={<SignUpMentor />} />
           <Route path="/cadets/join" element={<SignUpCadet />} />
-          {/* 모달 테스트용 URL -> 추후 멘토 상세페이지에 붙일 예정 */}
-          <Route
-            path="/mentors/info"
-            element={
-              <MentorInfoModal
-                intraId="m-engeng"
-                modalType={ModalType.MENTOR_INFO}
-              />
-            }
-          />
-          <Route
-            path="/mentors/time"
-            element={
-              <MentorInfoModal
-                intraId="m-engeng"
-                modalType={ModalType.AVAILABLE_TIME}
-              />
-            }
-          />
         </Routes>
         <Footer />
       </Router>
