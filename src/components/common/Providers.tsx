dda6592b21c1885ade54ab391ffa2644ef8e0eef
SeoJunhwan <seojunhwan@kakao.com>
2022-11-18 16:58:48 +0900

    design: themeProvider

--- src/components/common/Providers.tsx
+++ src/components/common/Providers.tsx
@@ -2,7 +2,8 @@ import { ReactNode } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-import defaultTheme from '../../styles/theme';
+import defaultThemeV1 from '@/styles/theme';
+import defaultThemeV2 from '@/styles/themeV2';
 import { ThemeProvider } from 'styled-components';
 
 interface Props {
@@ -21,8 +22,10 @@ function Providers({ children }: Props) {
   return (
     <>
       <QueryClientProvider client={queryClient}>
-        <ThemeProvider theme={defaultTheme}>
-          <Router basename="/">{children}</Router>
+        <ThemeProvider theme={defaultThemeV1}>
+          <ThemeProvider theme={defaultThemeV2}>
+            <Router basename="/">{children}</Router>
+          </ThemeProvider>
         </ThemeProvider>
       </QueryClientProvider>
     </>
