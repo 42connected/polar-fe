3ab993d787505d04b75bdfd6af00dece69796866
Hyeonsol Kong <besyia0k0@cau.ac.kr>
2022-11-18 16:13:59 +0900

    Merge develop Issues: #370

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,19 @@ import GlobalStyle from '@/styles/GlobalStyle';
 import reportWebVitals from '@/src/reportWebVitals';
 import '@/assets/font/font.css';
 import { ThemeProvider } from 'styled-components';
-import defaultTheme from '@/styles/theme';
 import App from '@/src/App';
 import '@/src/index.css';
+import themeV1 from '@/styles/theme';
+import themeV2 from '@/styles/themeV2';
 import React from 'react';
 
 ReactDOM.render(
   <React.StrictMode>
     <GlobalStyle />
-    <ThemeProvider theme={defaultTheme}>
-      <App />
+    <ThemeProvider theme={themeV1}>
+      <ThemeProvider theme={themeV2}>
+        <App />
+      </ThemeProvider>
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root'),
--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -1,7 +1,83 @@
 import 'styled-components';
 
 declare module 'styled-components' {
-  export interface DefaultTheme {
+  export interface ThemeV2 {
+    shadow: {
+      defaultShadow: string;
+    };
+    colors: {
+      polarBlue: string;
+      backgroundPink: string;
+      brightBlue: string;
+      black: string;
+      brightGray: string;
+      inputBoxColor: string;
+      simpleGray: string;
+      darkGray: string;
+      red: string;
+      yellow: string;
+      green: string;
+      slightBlue: string;
+      white: string;
+      middleGray: string;
+      polarMiddleBlue: string;
+      backgroundGray: string;
+    };
+
+    fontWeight: {
+      weightRegular: string;
+      weightBold: string;
+      weightExtraBold: string;
+    };
+
+    pcFontSize: {
+      sizeSmall: string;
+      sizeExtraSmall: string;
+      sizeMedium: string;
+      sizeExtraMedium: string;
+      sizeLarge: string;
+      sizeExtraLarge: string;
+    };
+    mobileFontSize: {
+      sizeSmall: string;
+      sizeMedium: string;
+      sizeLarge: string;
+    };
+
+    font: {
+      sebangGothic: string;
+      nanumGothic: string;
+    };
+
+    typography: {
+      bigSebang: string;
+      middleSebang: string;
+      smallSebang: string;
+      boldBigSebang: string;
+      boldMiddleSebang: string;
+      boldSmallSebang: string;
+      bigNanum: string;
+      middleNanum: string;
+      smallNanum: string;
+      boldBigNanum: string;
+      boldMiddleNanum: string;
+      boldSmallNanum: string;
+      ExtraBoldBigNanum: string;
+      ExtraBoldMiddleNanum: string;
+      ExtraBoldSmallNanum: string;
+    };
+
+    zIndex: {
+      header: string;
+      footer: string;
+      headerNav: string;
+      modal: string;
+      loading: string;
+      error: string;
+    };
+  }
+
+  export interface ThemeV1 {
     shadow: {
       defaultShadow: string;
       buttonShadow: string;
